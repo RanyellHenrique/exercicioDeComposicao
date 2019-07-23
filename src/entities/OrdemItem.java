f0f259eab0128bdb10f77957887a6356915c599a
RanyellHenrique <ranyellhenrique@gmail.com>
2019-07-23 14:59:07 -0300

    Criacao de construtores

--- src/entities/OrdemItem.java
+++ src/entities/OrdemItem.java
@@ -2,4 +2,38 @@ package entities;
 
 public class OrdemItem {
 
+		private Integer quantity;
+		private Double price;
+		
+		public OrdemItem() {
+		}
+
+		public OrdemItem(Integer quantity, Double price) {
+			this.quantity = quantity;
+			this.price = price;
+		}
+
+		public Integer getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(Integer quantity) {
+			this.quantity = quantity;
+		}
+
+		public Double getPrice() {
+			return price;
+		}
+
+		public void setPrice(Double price) {
+			this.price = price;
+		}
+		
+		public double subTotal() {
+			return price * quantity;
+		}
+		
+		
+		
+		
 }
